b12a8123460661ea11ad4d4b67505377a856baff
Владислав Чернявский <cernavskijv877@gmail.com>
2026-08-08 21:52:41 +1000

    Add load_dotenv.

--- src/zadan3django/settings.py
+++ src/zadan3django/settings.py
@@ -1,7 +1,11 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+load_dotenv(BASE_DIR.parent / '.env')
 
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'test_django_key')
